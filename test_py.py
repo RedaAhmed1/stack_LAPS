d0af740c1f665ea9649ccf1e33bb7bc0b9ba24e5
RedaAhmed1 <141954429+RedaAhmed1@users.noreply.github.com>
2025-09-25 20:07:18 +0300

    Add files via upload

---
+++ test_py.py
@@ -0,0 +1,32 @@
+
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def put(self,num):
+        self.queue.insert(0,num)
+    def get(self):
+        if len(self.queue) > 0:
+            num = self.queue[-1]
+            del self.queue[-1]
+            return num
+        else:
+            raise Exception
+
+
+class SuperQueue(Queue):
+    def isempty(self):
+        return len(self.queue) == 0
+
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("ERRRRRRRRRRRRRRRRRRRRRReda")
+    
